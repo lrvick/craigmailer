ce7e1906af561963d88dba1d79589af0215d3d3c
Lance Reagan Vick <lance@reaganvick.com>
2010-11-10 17:34:25 -0500

    Rewrite to use sqlite and fixed a bunch of things

--- craigmailer.php
+++ craigmailer.php
@@ -1,85 +1,64 @@
-<?php // CraigMailer v0.9
-$log_dir        = '/home/rvick/projects/craigmailer/';  // directory where logs should be stored
-$host           = "ssl://smtp.gmail.com";               // smtp server
-$port           = "465";                                // smtp port
-$username       = "lance@reaganvick.com";               // smtp username
-$password       = "lRv710";                             // smtp password
-$craigs_urls    = array(                                // list of urls (formatted as an array)
-		'http://orlando.craigslist.org/cpg', 
-		'http://orlando.craigslist.org/sad',
-		'http://orlando.craigslist.org/web',
-		'http://orlando.craigslist.org/sof',
-		'http://orlando.craigslist.org/eng'
-		);
-require_once "Mail.php";
-if (!file_exists($log_dir."last_url.txt")) {
-	touch($log_dir."last_url.txt");
-	$lastfile = array();
-} else {
-	$lastfile = file($log_dir."last_url.txt", FILE_IGNORE_NEW_LINES);
+<?php // CraigMailer
+$to_email    = 'lance@lrvick.net';                   // the email all postings should be sent to
+$sqlite_file = 'craigmailer.sqlite';                 // sqlite file location
+$city        = 'orlando';                            // city to watch (as per cl subdomain)
+$topics      = array('cpg','sad','web','sof','eng'); // topics (three char topics as used in cl urls)
+
+$handle = sqlite_open($sqlite_file, 0666, $sqlite_error) or die("Could not open database");
+$query = sqlite_query($handle,"SELECT name FROM sqlite_master WHERE type='table'");
+if (!sqlite_fetch_array($query)){
+	sqlite_query($handle,"CREATE TABLE posts (id INTEGER(15) PRIMARY KEY NOT NULL,timestamp DATETIME NOT NULL,topic CHAR(4) NOT NULL, subject CHAR(255) NOT NULL, email CHAR(255) NOT NULL, body TEXT, mail_sent INTEGER(1) DEFAULT 0 NOT NULL);")
+		or die("Error in query: ".sqlite_error_string(sqlite_last_error($handle)));
 }
-
-if (!file_exists($log_dir."craiglock")){
-	touch($log_dir."craiglock");
-	foreach ($craigs_urls as $watch_url) {
-		$rss = file_get_contents($watch_url.'/index.rss');
-		preg_match_all('#'.$watch_url.'/\d+\.html#', $rss, $urls_loop);
-		$urls = array_unique($urls_loop[0]);
-		$last_url .= $urls[0]."\n";
-		file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | last urls: $debug_last_url ; starting sends\n", FILE_APPEND);
-		foreach ($urls as $url) {
-			//Old style
-			//if (!in_array($url, $lastfile)) {
-
-			//New hotness
-			//Does craigslist delete posts? This is more robust, either way.
-			$url_path = parse_url($url, PHP_URL_PATH);
-			$url_pathinfo = pathinfo($url_path);
-			if (!$url_pathinfo['filename'] <= basename($lastfile[array_search($url_pathinfo['dirname'], $lastfile)], ".html")) {
-				$page = file_get_contents($url);
-				preg_match_all('#[-+.\w]+((\@)|(.{,4}at.{,4}))[-+.\w]+\.[[:alpha:]]{2,3}#', $page, $emails);
-				preg_match('#mailto\:(.*?)\?#', $page, $mailto);
-				preg_match('#<h2>(.*)</h2>#', $page, $header);
-				if (end($emails[0])) {
-					$mailto = html_entity_decode(end($emails[0]));
-				} elseif (end($mailto)) {
-					$mailto = html_entity_decode(end($mailto));
-				} else {
-					$mailto = FALSE;
-					file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | $url | not sent - no email address was listed.\n", FILE_APPEND);
-				}
-				if ($mailto) {
-					preg_match('#<div id=\"userbody\">(.*)</div>#msU', $page, $body);
-					$body = $body[1]."<a href=\"".$url."\">$url</a>";
-					$from = "\"".$mailto."\" <".$mailto.">";
-					$to = $username;
-					$subject = "craigslist:".$header[1]; 
-					$headers = array ('From' => $from, 'To' => $to, 'Subject' => $subject, 'Reply-To' => $mailto, 'Content-type' => 'text/html');
-					$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-					$mail = $smtp->send($to, $headers, $body);
-					file_put_contents($log_dir."unsent_mails", "\n--------------------\n".date('d-m-Y H:i:s').$headers."\n-----\n$body\n--------------------\n", FILE_APPEND);
-					/**if (PEAR::isError($mail)) {
-						file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | $url | $subject | error : ".$mail->getMessage()."\n", FILE_APPEND);
-						continue;
-					} else {
-						file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | $url | $subject | sent \n", FILE_APPEND);
-						continue;
-					}**/
-				}
-			} ELSE {
-				file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | $url in last urls - skipping to next feed. \n", FILE_APPEND);
-				break 2;
-			}
+foreach ($topics as $topic) {
+	$watch_url = "http://".$city.".craigslist.org/".$topic;
+	$rss = file_get_contents($watch_url."/index.rss");
+	preg_match_all('#'.$watch_url.'/\d+\.html#', $rss, $urls_loop);
+	$urls = array_unique($urls_loop[0]);
+	foreach ($urls as $url) {
+		$url_path = parse_url($url, PHP_URL_PATH);
+		$url_pathinfo = pathinfo($url_path);
+		$id =  $url_pathinfo['filename'];
+    $query = sqlite_query($handle,"SELECT * FROM posts WHERE id='$id'");
+    if (!sqlite_fetch_array($query)){
+		  $page = file_get_contents($url);
+      preg_match('#<div id=\"userbody\">(.*)</div>#msU', $page, $body);
+		  preg_match('#Date: (.*) EDT#msU', $page, $timestamp);
+		  $timestamp = explode(',',str_replace(' ','',$timestamp[1]));
+		  $timestamp = $timestamp[0]." ".date( 'H:i:s', strtotime($timestamp[1]));
+		  preg_match_all('#[-+.\w]+((\@)|(.{,4}at.{,4}))[-+.\w]+\.[[:alpha:]]{2,3}#', $page, $emails);
+		  preg_match('#mailto\:(.*?)\?#', $page, $mailto);
+		  preg_match('#<h2>(.*)</h2>#', $page, $header);
+		  if (end($emails[0])) {
+			  $email = html_entity_decode(end($emails[0]));
+		  } elseif (end($mailto)) {
+			  $email = html_entity_decode(end($mailto));
+		  }
+		  $body = sqlite_escape_string($body[1]."<a href=\"".$url."\">$url</a>");
+		  $subject = sqlite_escape_string($header[1]);
+		  if ($id && $timestamp && $topic && $subject && $email && $body) {
+		    $query = "INSERT INTO posts (id,timestamp,topic,subject,email,body) VALUES ('$id','$timestamp','$topic','$subject','$email','$body');";
+		    sqlite_query($handle,$query) or die("Error in query: ".sqlite_error_string(sqlite_last_error($handle)));
+		    echo "\n Added - ".$id." | ".$timestamp." | ".$topic." | ".$subject." | ".$email."\n";
+		  }
 		}
-		file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | Continuing to next feed from $urls[0]\n", FILE_APPEND);
 	}
-	file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | DONE. Setting last urls for feeds\n", FILE_APPEND);
-	file_put_contents($log_dir."last_url.txt", $last_url);
-	unlink($log_dir."craiglock");
-} ELSE {
-	file_put_contents($log_dir."craigmailer.log", date('d-m-Y H:i:s')." | Directory locked: Check for frozen script.\n", FILE_APPEND);
-	die("Lockfile exists");
 }
-
-
+$query = sqlite_query($handle,"SELECT * FROM posts WHERE mail_sent='0';") or die("Error in query: ".sqlite_error_string(sqlite_last_error($handle)));
+while ($post = sqlite_fetch_array($query)){
+	$id = $post[0];
+	$timestamp = $post[1];
+  $topic = $post[2];
+	$subject = "cl/".$topic." ".$post[3];
+	$email = $post[4];
+	$body = $timestamp."\n ".$post[5];
+  $headers  = 'From: '.$email."\r\n";
+  $headers .= 'Subject: '.$subject."\r\n";
+	$headers .= 'Reply-To: '.$email."\r\n";
+	//echo "\n".$body."\n";
+	if (mail($to_email, $subject, $body, $headers)){
+	  echo "\n mail sent for post #".$id."\n";
+	  sqlite_query($handle,"UPDATE posts SET mail_sent = '1' WHERE id='$id'") or die("Error in query: ".sqlite_error_string(sqlite_last_error($handle)));
+	}
+}
 ?>
